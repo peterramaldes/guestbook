aa73d91a47b35bf680d9779bd8ae3586cb2aa5d7
Peter A. Ramaldes <peter.ramaldes@gmail.com>
2019-08-23 23:11:16 -0300

    Criado de teste integrado.

--- test/clj/guestbook/test/db/core.clj
+++ test/clj/guestbook/test/db/core.clj
@@ -16,22 +16,20 @@
     (migrations/migrate ["migrate"] (select-keys env [:database-url]))
     (f)))
 
-(deftest test-users
+(deftest test-message
   (jdbc/with-db-transaction [t-conn *db*]
-    (jdbc/db-set-rollback-only! t-conn)
-    (is (= 1 (db/create-user!
-               t-conn
-               {:id         "1"
-                :first_name "Sam"
-                :last_name  "Smith"
-                :email      "sam.smith@example.com"
-                :pass       "pass"})))
-    (is (= {:id         "1"
-            :first_name "Sam"
-            :last_name  "Smith"
-            :email      "sam.smith@example.com"
-            :pass       "pass"
-            :admin      nil
-            :last_login nil
-            :is_active  nil}
-           (db/get-user t-conn {:id "1"})))))
+                            (jdbc/db-set-rollback-only! t-conn)
+                            (let [timestamp (java.time.LocalDateTime/now)]
+                              (is (= 1 (db/save-message!
+                                         t-conn
+                                         {:name      "Peter Admilson Ramaldes"
+                                          :message   "Primeiro Projeto com Clojure Luminus!"
+                                          :timestamp timestamp}
+                                         {:connection t-conn})))
+                              (is (=
+                                    {:name      "Peter Admilson Ramaldes"
+                                     :message   "Primeiro Projeto com Clojure Luminus!"
+                                     :timestamp timestamp}
+                                    (-> (db/get-messages t-conn {})
+                                        (first)
+                                        (select-keys [:name :message :timestamp])))))))
